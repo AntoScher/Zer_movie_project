d5d7c6d9a82795972fff348204fbfc667beb31c3
AntoScher <asc8er@gmail.com>
2025-01-11 17:31:51 +0300

    pre final

--- movie_project/films/urls.py
+++ movie_project/films/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+"""
 from . import views
-
 urlpatterns = [
     path('', views.index)
+]"""
+
+from django.urls import path
+from .views import movie_create_view, movie_list_view
+
+urlpatterns = [
+    path('', movie_create_view, name='movie_create'),
+    path('movie_list/', movie_list_view, name='movie_list'),
 ]
--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-
-# Create your views here.
+"""
 from django.http import HttpResponse
-
 def index(request):
     return HttpResponse("<h1>Это  проект movie_project созданный на Django</h1>")
+"""
+
+from django.shortcuts import render, redirect
+from .models import Movie
+from .forms import MovieForm
+
+def movie_create_view(request):
+    if request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('movie_list')
+    else:
+        form = MovieForm()
+    return render(request, 'movie_create.html', {'form': form})
+
+def movie_list_view(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_list.html', {'movies': movies})
